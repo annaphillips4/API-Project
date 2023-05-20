ea4217280aedb83f9bc1f811b7c36fe338c41fbd
Anna <annaphillips416@gmail.com>
2023-05-20 18:08:35 -0400

    types fixed in api/notes_routes

--- app/api/note_routes.py
+++ app/api/note_routes.py
@@ -10,7 +10,7 @@ note_routes = Blueprint('notes', __name__)
 @note_routes.route('')
 @login_required
 def notes():
-    all_notes = Notes.query.filter(Note.user_id == current_user.id).all()
+    all_notes = Note.query.filter(Note.user_id == current_user.id).all()
     return {note.id: note.to_dict() for note in all_notes}
 
 @note_routes.route('/', methods=['POST'])
@@ -29,7 +29,7 @@ def new_note():
 @note_routes.route('/<int:note_id>', methods=['PUT'])
 @login_required
 def edit_note(note_id):
-    form - NoteForm()
+    form = NoteForm()
     form['csrf_token'].data = request.cookies['csrf_token']
 
     note = Note.query.filter(Note.id == note_id, Note.user_id == current_user.id).first()
@@ -38,8 +38,8 @@ def edit_note(note_id):
     elif form.validate_on_submit():
         note.name = form.data['name']
         note.content = form.data['content']
-        note.public = form.date['public']
-        note.updated_at = datetime.now
+        note.public = form.data['public']
+        note.updated_at = datetime.now()
         db.session.commit()
         return note.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
@@ -51,5 +51,5 @@ def delete_note(note_id):
     if note:
         db.session.delete(note)
         db.session.commit()
-        return jsonify({'message': f'Note with id of {note_id} deleted successfuly.'}), 200
+        return jsonify({'message': f'Note with id of {note_id} deleted successfully.'}), 200
     return jsonify({'error': f'Note with id of {note_id} not found for user {current_user.username}.'}), 404
