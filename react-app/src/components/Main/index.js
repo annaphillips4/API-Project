680f89c627457a68eb153f92dfc3fdd9e8abdde2
Anna <annaphillips416@gmail.com>
2023-05-21 16:29:40 -0400

    moved unused modal components into deadcomponents. delete when project is finished

--- react-app/src/components/Main/index.js
+++ react-app/src/components/Main/index.js
@@ -3,7 +3,6 @@ import { useSelector } from "react-redux";
 import { Redirect } from 'react-router-dom/cjs/react-router-dom.min';
 import Notebooks from '../Notebooks'
 import Notes from '../Notes'
-import Navigation from '../Navigation';
 import UserBar from '../UserBar'
 
 function Main() {
